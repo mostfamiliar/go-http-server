1e59203461b1f655047273a6bf14519cb153533b
torrence <tlstratton@gmail.com>
2020-01-31 17:49:24 -0800

    Setup server and test

---
+++ http_server.go
@@ -0,0 +1,24 @@
+package serve
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func serve(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", serve)
+	http.HandleFunc("/headers", headers)
+	http.ListenAndServe(":8090", nil)
+}
